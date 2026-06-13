98e25085f286f94bc1660d80400a33705ac23b8f
Namitakumari1 <137058713+Namitakumari1@users.noreply.github.com>
2026-06-13 08:02:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -29,6 +29,17 @@ const questions = [
     answer: "Ottawa",
   },
 ];
+const questionsElement = document.getElementById("questions");
+const submitBtn = document.getElementById("submit");
+const scoreElement = document.getElementById("score");
+
+let userAnswers = JSON.parse(sessionStorage.getItem("progress")) || [];
+
+// Show previous score if it exists
+const savedScore = localStorage.getItem("score");
+if(savedScore !== null){
+    scoreElement.textContent = `Your score is ${savedScore} out of 5.`;
+}
 
 // Display the quiz questions and choices
 function renderQuestions() {
@@ -54,3 +65,30 @@ function renderQuestions() {
   }
 }
 renderQuestions();
+
+questionsElement.addEventListener("change", (e) => {
+    if(e.target.type === "radio"){
+        const index = Number(e.target.name.split("-")[1]);
+
+        userAnswers[index] = e.target.value;
+
+        sessionStorage.setItem(
+            "progress",
+            JSON.stringify(userAnswers)
+        );
+    }
+});
+
+submitBtn.addEventListener("click", () => {
+    let score = 0;
+
+    for(let i = 0; i < questions.length; i++){
+        if(userAnswers[i] === questions[i].answer){
+            score++;
+        }
+    }
+
+    scoreElement.textContent = `Your score is ${score} out of 5.`;
+
+    localStorage.setItem("score", score);
+});
